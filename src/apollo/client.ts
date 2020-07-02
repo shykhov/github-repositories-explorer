85dfca458a961bf2be298e5082fe46039e7a6100
Daniil Shykhov <Daniil_Shykhov@epam.com>
2020-07-02 18:08:51 +0300

    story/gsr-8: Rename process env token variable

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -6,13 +6,13 @@ export interface ProcessEnv {
   [key: string]: string | undefined;
 }
 
-const { REACT_APP_GH_AUTH_TOKEN = '', REACT_APP_GH_AUTH_LOCAL_TOKEN = '' }: ProcessEnv = process.env;
+const { REACT_APP_JWT_TOKEN = '', REACT_APP_GH_AUTH_LOCAL_TOKEN = '' }: ProcessEnv = process.env;
 
 export const client = new ApolloClient({
   cache,
   uri: 'https://api.github.com/graphql',
   headers: {
-    Authorization: `bearer ${REACT_APP_GH_AUTH_TOKEN || REACT_APP_GH_AUTH_LOCAL_TOKEN}`,
+    Authorization: `bearer ${REACT_APP_JWT_TOKEN || REACT_APP_GH_AUTH_LOCAL_TOKEN}`,
     Accept: 'application/vnd.github.machine-man-preview+json',
   },
 });
