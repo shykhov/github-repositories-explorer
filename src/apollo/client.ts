48ab2bf20a19ca61a639a9954e31ac15a98c2c12
Daniil Shykhov <Daniil_Shykhov@epam.com>
2020-07-02 17:56:50 +0300

    story/gsr-8: Resolve process env issue with token

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -3,16 +3,16 @@ import ApolloClient from 'apollo-boost';
 import { cache } from './fragment-matcher';
 
 export interface ProcessEnv {
-  [REACT_APP_GH_AUTH_TOKEN: string]: string | undefined;
+  [key: string]: string | undefined;
 }
 
-const { REACT_APP_GH_AUTH_TOKEN = '' }: ProcessEnv = process.env;
+const { REACT_APP_GH_AUTH_TOKEN = '', REACT_APP_GH_AUTH_LOCAL_TOKEN = '' }: ProcessEnv = process.env;
 
 export const client = new ApolloClient({
   cache,
   uri: 'https://api.github.com/graphql',
   headers: {
-    Authorization: `bearer ${REACT_APP_GH_AUTH_TOKEN}`,
+    Authorization: `bearer ${REACT_APP_GH_AUTH_TOKEN || REACT_APP_GH_AUTH_LOCAL_TOKEN}`,
     Accept: 'application/vnd.github.machine-man-preview+json',
   },
 });
