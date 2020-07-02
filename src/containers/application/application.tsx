6d5880a5bce57faeceae07c4008ce803b710f047
Daniil Shykhov <Daniil_Shykhov@epam.com>
2020-07-02 05:42:36 +0300

    story/gsr-8: Revert router history changes

--- src/containers/application/application.tsx
+++ src/containers/application/application.tsx
@@ -1,16 +1,11 @@
 import React, { FC } from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
 
 import { HomeContainer } from '../../pages/home';
 import { REPOSITORIES_PATHNAME } from '../../constants';
 
-const history = createBrowserHistory({
-  basename: process.env.PUBLIC_URL,
-});
-
 export const Application: FC = () => (
-  <BrowserRouter history={history}>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Switch>
       <Route exact component={HomeContainer} path={REPOSITORIES_PATHNAME} />
       <Redirect to={REPOSITORIES_PATHNAME} />
